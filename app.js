4008777a06ae5c8843ac67d43aca01e589bccbd0
Aritra62 <aritraghosh62@gmail.com>
2020-07-01 11:09:13 +0530

    !st Commit

--- app.js
+++ app.js
@@ -182,11 +182,6 @@ app.post("/login", function (req, res) {
     });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
-
-app.listen(port, function () {
+app.listen(3000, function () {
     console.log("Server has started succesfully");
 });
